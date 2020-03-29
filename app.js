415446720315c039c4e923481f8c904ce056833e
Valentin Silvera <valentinsilvera@icloud.com>
2020-03-29 11:51:53 +0200

    Created basic layout

---
+++ app.js
@@ -0,0 +1,15 @@
+$("#parallax_container").mousemove(function(e) {
+  parallaxIt(e, ".slide", -100);
+  parallaxIt(e, "img", -30);
+});
+
+function parallaxIt(e, target, movement) {
+  var $this = $("#bottom_index");
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+
+  TweenMax.to(target, 1, {
+    x: ((relX - $this.width() / 2) / $this.width()) * movement,
+    y: ((relY - $this.height() / 2) / $this.height()) * movement
+  });
+}
