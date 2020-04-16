9ec9caf9e54b3eb3e5959947ca7246f94b0468b3
Valentin Silvera <valentinsilvera@icloud.com>
2020-04-16 15:20:28 +0200

    Parallax effect done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,12 @@
-$("#container").mousemove(function(e) {
-  parallaxIt(e, ".slide", -100);
-  parallaxIt(e, "img", -30);
-});
+document.addEventListener("mousemove", parallax);
 
-function parallaxIt(e, target, movement) {
-  var $this = $("#container");
-  var relX = e.pageX - $this.offset().left;
-  var relY = e.pageY - $this.offset().top;
-
-  TweenMax.to(target, 1, {
-    x: ((relX - $this.width() / 2) / $this.width()) * movement,
-    y: ((relY - $this.height() / 2) / $this.height()) * movement
+function parallax(e) {
+  this.querySelectorAll(".layer").forEach((layer) => {
+    let speed = layer.getAttribute("data-speed");
+    let x = (window.innerWidth - e.pageX * speed) / 100;
+    let y = (window.innerWidth - e.pageY * speed) / 100;
+    layer.style.transform = `
+          translateX(${x}px)
+          translateY(${y}px)`;
   });
 }
