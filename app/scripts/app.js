aa323c7b37ff1320d6f43231777dc8392198994b
Valentin Silvera <valentinsilvera@icloud.com>
2020-04-17 11:56:12 +0200

    Added popup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,3 +10,21 @@ function parallax(e) {
           translateY(${y}px)`;
   });
 }
+
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  //fixes de body element
+  document.body.style.position = "fixed";
+  document.body.style.top = `-${window.scrollY}px`;
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  //return to previous place on the body
+  const top = document.body.style.top;
+  document.body.style.position = "";
+  document.body.style.top = "";
+  window.scrollTo(0, parseInt(scrollY || "0") * -1);
+}
